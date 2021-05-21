48a4f1a1aa81d5c94f0c8e5a5a44a6b9acbe9443
Taisei Toya <tytaisei0102@gmail.com>
2021-05-21 18:20:55 +0200

    feat(mariadb): mariadbと接続できるよう変更

--- srcs/wordpress/srcs/wp-config.php
+++ srcs/wordpress/srcs/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wpuser' );
 define( 'DB_PASSWORD', 'wppassword' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
