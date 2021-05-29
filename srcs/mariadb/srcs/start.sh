e7fa0b6cdf8b1926deeef3a8985d8c2791afad63
Taisei Toya <tytaisei0102@gmail.com>
2021-05-29 14:08:26 +0200

    feat(mariadb): shファイルを環境変数で作動させるよう変更

--- srcs/mariadb/srcs/start.sh
+++ srcs/mariadb/srcs/start.sh
@@ -3,9 +3,9 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql' & sleep 5
 
-mysql -u root -e "CREATE DATABASE wpdb;"
-mysql -u root -e "GRANT all privileges on wpdb.* to 'wpuser'@'%' identified by 'wppassword';"
-mysql -u root -e "USE wpdb; "
+mysql -u root -e "CREATE DATABASE $WP_DB_NAME;"
+mysql -u root -e "GRANT all privileges on $WP_DB_NAME.* to '$WP_DB_USER'@'%' identified by '$WP_DB_PASS';"
+mysql -u root -e "USE $WP_DB_NAME; "
 mysql -u root -e "FLUSH PRIVILEGES;"
 
 tail -f /dev/null
