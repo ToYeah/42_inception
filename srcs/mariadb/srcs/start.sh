56c82adbdfe0b6a94b067f0a16643d89becbbaab
Taisei Toya <tytaisei0102@gmail.com>
2021-05-30 13:58:19 +0200

    feat(mariadb): mariadbのユーザーを追加

--- srcs/mariadb/srcs/start.sh
+++ srcs/mariadb/srcs/start.sh
@@ -5,6 +5,8 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
 mysql -u root -e "CREATE DATABASE $WP_DB_NAME;"
 mysql -u root -e "GRANT all privileges on $WP_DB_NAME.* to '$WP_DB_USER'@'%' identified by '$WP_DB_PASS';"
+mysql -u root -e "GRANT all privileges on *.* to '$DB_USER_NAME_1'@'%' identified by '$DB_USER_PASS_1';"
+mysql -u root -e "GRANT all privileges on $WP_DB_NAME.* to '$DB_USER_NAME_2'@'%' identified by '$DB_USER_PASS_2';"
 mysql -u root -e "USE $WP_DB_NAME; "
 mysql -u root -e "FLUSH PRIVILEGES;"
 
