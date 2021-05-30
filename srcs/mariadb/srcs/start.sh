65a369072c93945b34145c269dc395cceb28750e
Taisei Toya <tytaisei0102@gmail.com>
2021-05-30 17:30:06 +0200

    feat(mariadb): mysqlをフォアグラウンドで実行するよう変更

--- srcs/mariadb/srcs/start.sh
+++ srcs/mariadb/srcs/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
+mysql_install_db --user=mysql --datadir=/var/lib/mysql 
 
-/usr/bin/mysqld_safe --datadir='/var/lib/mysql' & sleep 5
+cat << EOF > /tmp/init.sql
+CREATE DATABASE $WP_DB_NAME;
+GRANT all privileges on $WP_DB_NAME.* to '$WP_DB_USER'@'%' identified by '$WP_DB_PASS';
+GRANT all privileges on *.* to '$DB_USER_NAME_1'@'%' identified by '$DB_USER_PASS_1';
+GRANT all privileges on $WP_DB_NAME.* to '$DB_USER_NAME_2'@'%' identified by '$DB_USER_PASS_2';
+USE $WP_DB_NAME; 
+FLUSH PRIVILEGES;
+EOF
 
-mysql -u root -e "CREATE DATABASE $WP_DB_NAME;"
-mysql -u root -e "GRANT all privileges on $WP_DB_NAME.* to '$WP_DB_USER'@'%' identified by '$WP_DB_PASS';"
-mysql -u root -e "GRANT all privileges on *.* to '$DB_USER_NAME_1'@'%' identified by '$DB_USER_PASS_1';"
-mysql -u root -e "GRANT all privileges on $WP_DB_NAME.* to '$DB_USER_NAME_2'@'%' identified by '$DB_USER_PASS_2';"
-mysql -u root -e "USE $WP_DB_NAME; "
-mysql -u root -e "FLUSH PRIVILEGES;"
-
-tail -f /dev/null
+/usr/bin/mysqld_safe --datadir='/var/lib/mysql' --init-file=/tmp/init.sql
