030900eac4366a1dcc3dba2fc917f498514e3dae
Taisei Toya <tytaisei0102@gmail.com>
2021-07-03 11:52:33 +0200

    style(mariadb/start.sh): クエリを大文字に変更

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -4,9 +4,9 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql --auth-root-authenticatio
 cat << EOF > /tmp/init.sql
 SET PASSWORD FOR root@localhost=password('$DB_ROOT_PASS');
 CREATE DATABASE $WP_DB_NAME;
-GRANT all privileges on $WP_DB_NAME.* to '$WP_DB_USER'@'%' identified by '$WP_DB_PASS';
-GRANT all privileges on *.* to '$DB_USER_NAME_1'@'%' identified by '$DB_USER_PASS_1';
-GRANT all privileges on $WP_DB_NAME.* to '$DB_USER_NAME_2'@'%' identified by '$DB_USER_PASS_2';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS';
+GRANT ALL PRIVILEGES ON *.* TO '$DB_USER_NAME_1'@'%' IDENTIFIED BY '$DB_USER_PASS_1';
+GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$DB_USER_NAME_2'@'%' IDENTIFIED BY '$DB_USER_PASS_2';
 USE $WP_DB_NAME; 
 FLUSH PRIVILEGES;
 EOF
